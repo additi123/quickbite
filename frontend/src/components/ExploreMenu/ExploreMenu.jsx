1f2c357e538388999bd62b9bf96f1f129aa77ff7
additi123 <mehtaadditi@gmail.com>
2024-08-22 15:10:35 -0400

    items

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,32 @@
  import React from 'react'
- import './ExploreMenu.css'
+ import './ExploreMenu.css' 
+ import { menu_list } from '../../assets/assets'
  
  const ExploreMenu = () => {
    return (
-     <div>
-       
-     </div>
+    <div className="explore-menu" id="explore-menu"> 
+
+    <h1>Explore our menu</h1>
+    <p className='explore-menu-text'>Dive into our menu to discover a world of flavors, from savory classics to exciting new dishes. Whether you’re craving something familiar or eager to try something fresh, our menu has something to satisfy every taste.</p>
+    <div className="explore-menu-list">
+        {menu_list.map((item,index)=> {
+            return (
+                <div key={index}  className="explore-menu-list-item">
+                    <img src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+
+
+
+    </div>
+    
+    
+    
+    
+    </div>
+   
    )
  }
  
