157a253320fbf748a2ba905cfe31b2d209ae25d0
additi123 <mehtaadditi@gmail.com>
2024-09-05 23:01:15 -0400

    explore menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,34 +1,31 @@
- import React from 'react'
- import './ExploreMenu.css' 
- import { menu_list } from '../../assets/assets'
- 
- const ExploreMenu = () => {
-   return (
-    <div className="explore-menu" id="explore-menu"> 
 
-    <h1>Explore our menu</h1>
-    <p className='explore-menu-text'>Dive into our menu to discover a world of flavors, from savory classics to exciting new dishes. Whether you’re craving something familiar or eager to try something fresh, our menu has something to satisfy every taste.</p>
-    <div className="explore-menu-list">
-        {menu_list.map((item,index)=> {
-            return (
-                <div key={index}  className="explore-menu-list-item">
-                    <img src={item.menu_image} alt="" />
-                    <p>{item.menu_name}</p>
-                </div>
-            )
-        })}
+import React from 'react'
+import './ExploreMenu.css' 
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = ({category, setCategory}) => {
+  return (
+   <div className="explore-menu" id="explore-menu"> 
+
+   <h1>Explore our menu</h1>
+   <p className='explore-menu-text'>Dive into our menu to discover a world of flavors, from savory classics to exciting new dishes. Whether you’re craving something familiar or eager to try something fresh, our menu has something to satisfy every taste.</p>
+   <div className="explore-menu-list">
+       {menu_list.map((item,index)=> {
+           return (
+               <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index}  className="explore-menu-list-item">
+                   <img className={category===item.menu_name? "active": ""} src={item.menu_image} alt="" />
+                   <p>{item.menu_name}</p>
+               </div>
+           )
+       })}
 
 
 
-    </div>
-    
-    
-    
-    
-    </div>
-   
-   )
- }
- 
- export default ExploreMenu
- 
+   </div>
+   </div>
+  
+  )
+}
+
+export default ExploreMenu
+
