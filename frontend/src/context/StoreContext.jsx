ea65eefebfbf9670663d78fbc46074ba8efa85a8
additi123 <mehtaadditi@gmail.com>
2024-09-15 11:56:59 -0400

    food listing

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,17 +1,26 @@
 import { createContext } from "react";
+import { food_list } from "../assets/assets"; 
+
 
  export const StoreContext = createContext(null)
 
  const StoreContextProvider = (props) => {
-    const contextvalue =  {
-
-
+    const contextValue =  {
+        food_list
     }
 
+    
+
     return (
-        
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+
+             
+        </StoreContext.Provider>
     )
 
 
      
  }
+
+ export default StoreContextProvider;
